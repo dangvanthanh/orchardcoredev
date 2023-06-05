a8d5e873f8ec57579537ed4c8e6b5d9b5aa4c503
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-06-05 20:25:45 +0700

    Initialize

---
+++ src/Modules/CodySchool.Core/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "CodySchool Core",
+    Author = "Dang Van Thanh",
+    Website = "https://dangthanh.org",
+    Version = "0.0.1",
+    Description = "CodySchool Core",
+    Category = "Content Management"
+)]
---
+++ src/Themes/CodySchool.Theme/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "CodySchool Theme",
+    Author = "Dang Van Thanh",
+    Website = "https://dangthanh.org",
+    Version = "0.0.1",
+    Description = "CodySchool Theme",
+    Tags = new[] { "Default" }
+)]
