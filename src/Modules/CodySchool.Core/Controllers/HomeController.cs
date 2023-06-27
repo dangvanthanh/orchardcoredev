aa1b3495a98e4d2c308e0ac759e4145dd745af0c
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-06-28 00:23:19 +0700

    Add new route and notify

--- src/Modules/CodySchool.Core/Controllers/HomeController.cs
+++ src/Modules/CodySchool.Core/Controllers/HomeController.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using OrchardCore.DisplayManagement.Notify;
-using System.Threading.Tasks;
-
 namespace CodySchool.Core.Controllers
 {
     public class HomeController : Controller
@@ -12,11 +10,33 @@ namespace CodySchool.Core.Controllers
         private readonly INotifier _notifier;
         private readonly IStringLocalizer T;
         private readonly IHtmlLocalizer H;
-
         private readonly ILogger _logger;
+
+        public HomeController(INotifier notifier,
+            IStringLocalizer<HomeController> stringLocalizer,
+            IHtmlLocalizer<HomeController> htmlLocalizer,
+            ILogger<HomeController> logger)
+        {
+            _notifier = notifier;
+            _logger = logger;
+
+            T = stringLocalizer;
+            H = htmlLocalizer;
+        }
 
         public ActionResult Index()
         {
+            ViewData["Message"] = T["CodySchool Core!"];
+            return View();
+        }
+
+        [Route("Home/Notify")]
+        public async Task<IActionResult> Notify()
+        {
+            _logger.LogError("You have been notified about some error!");
+
+            await _notifier.InformationAsync(H["Congratulations! You have been notified! Check the error log too!"]);
+
             return View();
         }
     }
