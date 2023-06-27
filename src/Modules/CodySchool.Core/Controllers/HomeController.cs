04e4d018e789277135d11d0b088ddf4aa19eeead
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-06-27 23:59:28 +0700

    Add first controller

--- src/Modules/CodySchool.Core/Controllers/HomeController.cs
+++ src/Modules/CodySchool.Core/Controllers/HomeController.cs
@@ -1,14 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using OrchardCore.DisplayManagement.Notify;
+using System.Threading.Tasks;
 
 namespace CodySchool.Core.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly INotifier _notifier;
+        private readonly IStringLocalizer T;
+        private readonly IHtmlLocalizer H;
+
+        private readonly ILogger _logger;
+
         public ActionResult Index()
         {
             return View();
